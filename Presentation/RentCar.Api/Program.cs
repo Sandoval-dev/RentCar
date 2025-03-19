674baa88236a2ab3f6dca94ac2d780cf3a169001
Osman Erdem Kilic <osman_erdem1200@hotmail.com>
2025-03-19 15:11:57 +0300

    Car Crud Endpointleri

--- Presentation/RentCar.Api/Program.cs
+++ Presentation/RentCar.Api/Program.cs
@@ -1,7 +1,13 @@
+using RentCar.Application.Services.CarServices;
+using RentCar.Persistence.Context;
+using RentCar.Persistence.Repositories.CarRepositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<RentCarDbContext>();
+builder.Services.AddScoped<ICarRepository, CarRepository>();
+builder.Services.AddScoped<ICarServices, CarServices>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
