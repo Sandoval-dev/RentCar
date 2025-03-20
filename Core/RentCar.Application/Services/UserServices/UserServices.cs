542279cf58c2a9302b0765ec2329cfd969251cd8
Osman Erdem Kilic <osman_erdem1200@hotmail.com>
2025-03-20 11:59:05 +0300

    User Servisleri

---
+++ Core/RentCar.Application/Services/UserServices/UserServices.cs
@@ -0,0 +1,89 @@
+﻿using RentCar.Application.Dtos.UserDtos;
+using RentCar.Domain.Entities;
+using RentCar.Persistence.Repositories.UserRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentCar.Application.Services.UserServices
+{
+    public class UserServices : IUserServices
+    {
+        private readonly IUserRepository _repository;
+
+        public UserServices(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task CreateUser(CreateUserDto dto)
+        {
+            var user = new User
+            {
+                Name = dto.Name,
+                Surname = dto.Surname,
+                Email = dto.Email,
+                Phone = dto.Phone,
+                Password = dto.Password,
+                Role = dto.Role,
+                RentedCars= new List<RentedCar>()
+            };
+            await _repository.CreateUserAsync(user);
+        }
+
+        public async Task DeleteUser(int id)
+        {
+            var value= await _repository.GetByIdUserAsync(id);
+            await _repository.DeleteUserAsync(value);
+        }
+
+        public async Task<List<ResultUserDto>> GetAllUsers()
+        {
+            var users=await _repository.GetAllUsersAsync();
+            var result = users.Select(x => new ResultUserDto
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Surname = x.Surname,
+                Email = x.Email,
+                Phone = x.Phone,
+                Password = x.Password,
+                Role = x.Role,
+                RentedCars = new List<RentedCar>()
+            }).ToList();
+            return result;
+        }
+
+        public async Task<GetByIdUserDto> GetByIdUser(int id)
+        {
+            var value=await _repository.GetByIdUserAsync(id);
+            var result = new GetByIdUserDto
+            {
+                Id = value.Id,
+                Name = value.Name,
+                Surname = value.Surname,
+                Email = value.Email,
+                Password = value.Password,
+                Phone = value.Phone,
+                Role = value.Role,
+                RentedCars = new List<RentedCar>()
+            };
+            return result;
+        }
+
+        public async Task UpdateUser(UpdateUserDto dto)
+        {
+           var user=await _repository.GetByIdUserAsync(dto.Id);
+            user.Name = dto.Name;
+            user.Surname = dto.Surname;
+            user.Email = dto.Email;
+            user.Password = dto.Password;
+            user.Phone = dto.Phone;
+            user.Role = dto.Role;
+
+            await _repository.UpdateUserAsync(user);
+
+        }
+    }
+}
---
+++ Presentation/RentCar.Api/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentCar.Application.Dtos.UserDtos;
+using RentCar.Application.Services.UserServices;
+
+namespace RentCar.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserServices _services;
+
+        public UserController(IUserServices services)
+        {
+            _services = services;
+        }
+
+        [HttpGet("getallusers")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var result = await _services.GetAllUsers();
+            return Ok(result);
+        }
+
+        [HttpGet("getbyiduser")]
+        public async Task<IActionResult> GetByIdUser(int id)
+        {
+            var result = await _services.GetByIdUser(id);
+            return Ok(result);
+        }
+
+        [HttpPost("createuser")]
+        public async Task<IActionResult> CreateUser(CreateUserDto dto)
+        {
+            await _services.CreateUser(dto);
+            return Ok("User created");
+        }
+
+        [HttpPut("updateuser")]
+        public async Task<IActionResult> UpdateUser(UpdateUserDto dto)
+        {
+            await _services.UpdateUser(dto);
+            return Ok("User updated");
+        }
+
+        [HttpDelete("deleteuser")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            await _services.DeleteUser(id);
+            return Ok("User deleted");
+        }
+    }
+}
